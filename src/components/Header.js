60740731fe6bbf6f5660cefcb66e0d5ca0312ba2
Cazper Sihvonen <cazperhjortstam@hotmail.com>
2020-06-22 10:04:38 +0200

    Homepage finished

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styling/Header.css'
 
 const Header = () => (
-    <>
+    <div className='header'>
         <Link to='/'>Home</Link>
-    </>
+        <Link to='/explore'>Explore</Link>
+    </div>
 )
 
 export default Header;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import '../styling/Home.css';
+import image from '../assets/images/landing-page-img.png'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div className='home'>
+            <h1>Welcome to the Cocktail Explorer!</h1>
+            <img src={image} />
+        </div>
     )
 }
 
