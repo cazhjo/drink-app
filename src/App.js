414088666681d15cea6d174cd1da4785f963f984
Cazper Sihvonen <cazperhjortstam@hotmail.com>
2020-06-22 09:48:31 +0200

    Added drinks.json and Header component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
+import Header from './components/Header'
 
 const App = () => (
     <BrowserRouter>
+        <div>
+        <Header />
         <Switch>
-            <Route path='/' component={Home} />
+            <Route path='/' component={Home} exact />
         </Switch>
+        </div>
     </BrowserRouter>
 )
 
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => (
+    <>
+        <Link to='/'>Home</Link>
+    </>
+)
+
+export default Header;
